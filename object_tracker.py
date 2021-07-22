70471e9f63eaa55b9fdf4e12365772e38a81e443
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-07-22 22:40:10 +0900

    Add Memory Growth Differ error in Multi-GPU
    In Multi-GPU environment, previous code occur `ValueError: Memory growth cannot differ between GPU devices`
    
    See more about tensorflow docs : https://www.tensorflow.org/guide/gpu

--- object_tracker.py
+++ object_tracker.py
@@ -5,6 +5,7 @@ import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
+    tf.config.experimental.set_visible_devices(physical_devices[0], 'GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
