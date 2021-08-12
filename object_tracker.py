ad077cb156278a74db479f656151291bf629d766
wei-chun-wang <40432877+william870702@users.noreply.github.com>
2021-08-13 00:22:26 +0800

    0813TASSA兩個CLASS

--- object_tracker.py
+++ object_tracker.py
@@ -34,7 +34,7 @@ flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.50, 'score threshold')
-flags.DEFINE_boolean('dont_show', False, 'dont show video output')
+flags.DEFINE_boolean('dont_show', True, 'dont show video output')
 flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
 flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 
